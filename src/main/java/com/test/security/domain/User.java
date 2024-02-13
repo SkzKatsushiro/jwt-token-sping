71a18b4cd3a80eb75ab5ba92ae3f5a08d14d9d21
SkzKatsushiro <SkzKatsushiro@users.noreply.github.com>
2024-02-13 10:45:36 +0000

    test

---
+++ src/main/java/com/test/security/domain/User.java
@@ -0,0 +1,31 @@
+package com.test.security.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "test-app-user", schema = "application-security")
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class User {
+
+    @Id
+    @GeneratedValue
+    Integer id;
+
+    String firstName;
+    String lastName;
+    String email;
+    String password;
+}
